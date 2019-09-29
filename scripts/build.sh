ca7094462897f6cb08a7f6f8b51f8b3a480a7fde
pacobarrachina <pacobarrachina.mari@gmail.com>
2019-09-29 22:08:57 +0200

    Un cambio en el archivo paquage.json

---
+++ scripts/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf C:/Users/Francisco/Documents/DAW 2/DespliegueAPP/NBA1/dist
+
+mkdir C:/Users/Francisco/Documents/DAW 2/DespliegueAPP/NBA1/dist
+
+cp -r C:/Users/Francisco/Documents/DAW 2/DespliegueAPP/NBA1/src/index.html C:/Users/Francisco/Documents/DAW 2/DespliegueAPP/NBA1/dist/index.html
---
+++ scripts/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf C:/Users/Francisco/Documents/DAW 2/DespliegueAPP/NBA1/dist/index.html
+
+cp -r C:/Users/Francisco/Documents/DAW 2/DespliegueAPP/NBA1/src/index.html C:/Users/Francisco/Documents/DAW 2/DespliegueAPP/NBA1/dist/index.html
+
+cp -r C:/Users/Francisco/Documents/DAW 2/DespliegueAPP/NBA1/dist/index.html C:/xampp/htdocs/despliegueAPP/nba1/src/index.html
